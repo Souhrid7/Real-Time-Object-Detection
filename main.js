054da05086655c22f1d19aaf7cefdcbda208802e
Souhrid7 <153090885+Souhrid7@users.noreply.github.com>
2024-02-07 17:51:44 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,27 @@
+function setup() {
+  canvas = createCanvas(300, 300);
+  canvas.center();
+  video = createCapture(VIDEO);
+  video.hide();
+  classifier = ml5.imageClassifier('MobileNet',modelLoaded)
+}
+function draw() {
+  image(video,0,0,300,300)
+  classifier.classify(video,gotResults)
+}
+function modelLoaded() {
+  console.log('ModelLoaded')
+}
+previous_result=''
+function gotResults(error,result) {
+  if(error) {
+    console.error(error)
+  }
+  else if(result[0].confidence>0.5 && previous_result!=result[0].label) {
+    console.log(result)
+    previous_result=result[0].label
+    document.getElementById('result_object_name').innerHTML="Object Name: "+result[0].label
+    document.getElementById('result_object_accuracy').innerHTML="Accuracy: "+(result[0].confidence.toFixed(3)*100)+" %"
+  }
+}
+
